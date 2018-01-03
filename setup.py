7b3407098213cb2c51723f35f747675e3c0e31da
Zena Hirsch <zena@vimeo.com>
2018-01-03 16:14:06 -0500

    add configparser

--- setup.py
+++ setup.py
@@ -3,13 +3,18 @@ from setuptools import setup
 
 setup(
     name='zenkly',
-    version='0.1',
+    version='0.1.1',
     py_modules=['zenkly'],
+    description='CLI for Zendesk admin tasks',
+    author='Zena Hirsch',
+    author_email='hirsch.zena@gmail.com',
+    url='https://github.com/zenahirsch/zenkly',
     install_requires=[
         'Click',
         'requests',
         'jsonschema',
         'colorama',
+        'configparser',
     ],
     entry_points='''
         [console_scripts]
